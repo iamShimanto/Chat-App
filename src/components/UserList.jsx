62486f38d0ca27f55001d22e2a31700897469e8d
iamShimanto <shimantowebdeveloper@gmail.com>
2025-06-21 10:20:51 +0600

    user profile

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -33,7 +33,7 @@ const UserList = ({ data }) => {
         </button>
       </div>
       {show && (
-        <div className="w-75 h-60 rounded-2xl bg-slate-50 py-10 my-2 mx-auto add">
+        <div className="w-75 h-60 overflow-y-auto rounded-2xl bg-slate-50 py-5 my-2 mx-auto add">
           <img
             className="w-20 h-20  rounded-full mx-auto"
             src={data.profile_picture}
